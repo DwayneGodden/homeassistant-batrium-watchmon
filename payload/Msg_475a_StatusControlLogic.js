176e7c8b48aa65546fc19b57b4103cd645ee3f1e
jwthecolorist <jwthecolorist@users.noreply.github.com>
2026-05-03 17:59:07 -0600

    Fix trailing spaces in binary-parser field names causing load errors

--- payload/Msg_475a_StatusControlLogic.js
+++ payload/Msg_475a_StatusControlLogic.js
@@ -35,58 +35,58 @@ const status = new Parser()
 			Limited Power 	= 2,
 			Normal Power  	= 4, */
 	.uint8('isChargePowerRateCalc')  		/* Choices ChargeRateStates */
-	.uint8('hasChargeCellVoltHi ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeCellVoltPause ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeCellVoltLimPower ')  	// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeCellTempLo ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeCellTempHi ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeAmbientTempLo ') 		// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeAmbientTempHi ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeSupplyVoltHi ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeSupplyVoltPause ')  	// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeShuntVoltHi ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeShuntVoltPause ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeShuntVoltLimPower ')  	// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeShuntSocHi ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeShuntSocPause ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeAboveInitalBypass ')	// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeAboveFinalBypass ')		// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeInBypass ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeBypassComplete ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeBypassTempRelief ')  	// Boolean 0 = Off , 1 = On
-	.uint8('hasChargeBypassSessionLo ')  	// Boolean 0 = Off , 1 = On
-	.uint8('isDischgOnState ')  				// Boolean 0 = Off , 1 = On
-	.uint8('isDischgLimPower ')  			// Boolean 0 = Off , 1 = On
-	.uint8('isDischgTransition ')  			// Boolean 0 = Off , 1 = On
-	.uint8('isDischgPowerRateState ') /* Choices DischgRateStates
+	.uint8('hasChargeCellVoltHi')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeCellVoltPause')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeCellVoltLimPower')  	// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeCellTempLo')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeCellTempHi')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeAmbientTempLo') 		// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeAmbientTempHi')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeSupplyVoltHi')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeSupplyVoltPause')  	// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeShuntVoltHi')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeShuntVoltPause')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeShuntVoltLimPower')  	// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeShuntSocHi')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeShuntSocPause')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeAboveInitalBypass')	// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeAboveFinalBypass')		// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeInBypass')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeBypassComplete')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeBypassTempRelief')  	// Boolean 0 = Off , 1 = On
+	.uint8('hasChargeBypassSessionLo')  	// Boolean 0 = Off , 1 = On
+	.uint8('isDischgOnState')  				// Boolean 0 = Off , 1 = On
+	.uint8('isDischgLimPower')  			// Boolean 0 = Off , 1 = On
+	.uint8('isDischgTransition')  			// Boolean 0 = Off , 1 = On
+	.uint8('isDischgPowerRateState') /* Choices DischgRateStates
 			Off 			= 0,
 			Limited Power 	= 2,
 			Normal Power  	= 4, */
-	.uint8('isDischgPowerRateCalc ')  		/* Choices DischgRateStates */
-	.uint8('hasDischgCellVoltLo ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgCellVoltPause ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgCellVoltLimPower ')  	// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgCellTempLo ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgCellTempHi ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgAmbientLo ')	  		// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgAmbientHi ') 			// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgSupplyVoltLo ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgSupplyVoltPause ')  	// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgShuntVoltLo ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgShuntVoltPause ')  		// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgShuntVoltLimPower ')  	// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgShuntSocLo ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasDischgShuntSocPause ')  		// Boolean 0 = Off , 1 = On
-	.uint8('isHeatOnState ')  				// Boolean 0 = Off , 1 = On
-	.uint8('isHeatOnCalc ')  				// Boolean 0 = Off , 1 = On
-	.uint8('isHeatTransition ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasHeatAmbientLo ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasHeatCellTempLo ')  			// Boolean 0 = Off , 1 = On
+	.uint8('isDischgPowerRateCalc')  		/* Choices DischgRateStates */
+	.uint8('hasDischgCellVoltLo')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgCellVoltPause')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgCellVoltLimPower')  	// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgCellTempLo')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgCellTempHi')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgAmbientLo')	  		// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgAmbientHi') 			// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgSupplyVoltLo')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgSupplyVoltPause')  	// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgShuntVoltLo')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgShuntVoltPause')  		// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgShuntVoltLimPower')  	// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgShuntSocLo')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasDischgShuntSocPause')  		// Boolean 0 = Off , 1 = On
+	.uint8('isHeatOnState')  				// Boolean 0 = Off , 1 = On
+	.uint8('isHeatOnCalc')  				// Boolean 0 = Off , 1 = On
+	.uint8('isHeatTransition')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasHeatAmbientLo')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasHeatCellTempLo')  			// Boolean 0 = Off , 1 = On
 	.uint8('isCoolOnState')  				// Boolean 0 = Off , 1 = On
 	.uint8('isCoolOnCalc')  					// Boolean 0 = Off , 1 = On
 	.uint8('isCoolTransition')  				// Boolean 0 = Off , 1 = On
-	.uint8('hasCoolAmbientHi ')  			// Boolean 0 = Off , 1 = On
-	.uint8('hasCoolCellTempHi ')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasCoolAmbientHi')  			// Boolean 0 = Off , 1 = On
+	.uint8('hasCoolCellTempHi')  			// Boolean 0 = Off , 1 = On
 
 module.exports = function(msg)
 {
